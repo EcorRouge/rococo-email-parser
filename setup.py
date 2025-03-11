8638eb1e1213d07e54a65cdb0f81092c082963ea
Mikhail Burilov <burilovmv@gmail.com>
2025-03-11 09:46:44 +0300

    missing chardet

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     install_requires=[
         'pydantic<3.0.0,>=2.1.0',
         'python-dateutil<3.0.0,>=2.1.0',
-        'beautifulsoup4==4.12.2'
+        'beautifulsoup4==4.12.2',
+        'chardet==5.2.0'
     ],
     python_requires=">=3.10"
 )
